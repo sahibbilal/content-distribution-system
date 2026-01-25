2296ac61bc1e5d1190202e55b118faca42a2d3e0
Bilal Mahmood <bilal.m@829llc.com>
2026-01-26 01:54:40 +0500

    updated 1 - 4

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,6 +32,29 @@
 </head>
 <body>
     <div id="app"></div>
+    
+    <!-- Facebook SDK for JavaScript -->
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '{{ env('FACEBOOK_APP_ID', '') }}',
+          cookie     : true,
+          xfbml      : true,
+          version    : 'v18.0'
+        });
+          
+        FB.AppEvents.logPageView();   
+          
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "https://connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+    </script>
 </body>
 </html>
 
